1de381fb94a0ea16fd9f2d7d8716cc78c913ee66
Vaibhav Arcot <yvarcot@gmail.com>
2019-02-09 14:28:40 -0500

    Added raw data A2

---
+++ estimate_rot.py
@@ -0,0 +1,11 @@
+#data files are numbered on the server.
+#for exmaple imuRaw1.mat, imuRaw2.mat and so on.
+#write a function that takes in an input number (1 through 6)
+#reads in the corresponding imu Data, and estimates 
+#roll pitch and yaw using an extended kalman filter
+
+
+
+def estimate_rot(data_num=1):
+	#your code goes here
+	return roll,pitch,yaw
